d99053f404eadcbeb0111830aaac5d751c0cd160
Figsh <safetyunion5550@gmail.com>
2025-09-28 20:43:03 +0100

    Update fcnfig.js

--- netlify/functions/fcnfig.js
+++ netlify/functions/fcnfig.js
@@ -1,18 +1,15 @@
-// netlify
+// netlify/functions/fcnfig.js
 exports.handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
       apiKey: process.env.PUBLIC_FIRE_API,
       authDomain: "devtemple0.firebaseapp.com",
-  projectId: "devtemple0",
-  storageBucket: "devtemple0.firebasestorage.app",
-  messagingSenderId: "70957783581",
-  appId: "1:70957783581:web:8abb8c66c4a7eb258244f4",
-  measurementId: "G-79T0JMS2KE"
+      projectId: "devtemple0",
+      storageBucket: "devtemple0.appspot.com",   // fix: should end with
+      messagingSenderId: "70957783581",
+      appId: "1:70957783581:web:8abb8c66c4a7eb258244f4",
+      measurementId: "G-79T0JMS2KE"
     }),
   };
 };
-
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
